28cb898656d9b4981469a7b288ce03d2e92f428d
brulo <bruce.lott@gmail.com>
2015-03-15 19:11:51 -0700

    Move from Lerp to RotateToward for pouring animation

--- Assets/Scripts/PourableDispenser.cs
+++ Assets/Scripts/PourableDispenser.cs
@@ -2,88 +2,83 @@ using UnityEngine;
 using System.Collections;
 
 public class PourableDispenser : StationaryDispenser {
-	public float tiltTime = 1f;
-	public float untiltTime = 0.5f;
+	public float tiltSpeed = 100f;
+	public float timeBetweenDispenses = 0.5f; 
+
 	private bool isTilting = false;
 	private bool isUntilting = false;
-	private Quaternion defaultRotation = Quaternion.Euler(0,0,0);
-	private Quaternion pouringRotation = Quaternion.Euler(0,0,90);
-	private float fullPourAngle;
+	private bool isPouring = false;
+	private Quaternion defaultRotation = Quaternion.Euler(0, 0, 0);
+	private Quaternion pouringRotation = Quaternion.Euler(0, 0, 90);
+	private float lastDispenseTime; 
 
-	// Shared lerp variables.
-	private Quaternion lerpStartRotation;
-	private float lerpStartTime;
-	private float lerpTime;
+	/* protected override void Start() { */
+	/* 	base.Start(); */
+	/* } */
 
-	protected override void Start() {
-		base.Start();
-		fullPourAngle = Quaternion.Angle(defaultRotation, pouringRotation);
-	}
-
-	void Update() {
+	void FixedUpdate() {
 		if(isTilting) {
-			bool finishedLerping = LerpIt(pouringRotation);
-			if(finishedLerping) {
-				Debug.Log("Finished Tilting");
+			DoRotateTowards(pouringRotation);
+			if(IsDoneRotatingTowards(pouringRotation)) {
 				isTilting = false;
-				Dispense();
+				StartPouring();
 			}
-		}	
+		}
 		if(isUntilting) {
-			bool finishedLerping = LerpIt(defaultRotation);
-			if(finishedLerping) {
-				Debug.Log("Finished Untilting");
+			DoRotateTowards(defaultRotation);
+			if(IsDoneRotatingTowards(defaultRotation)) {
 				isUntilting = false;
 			}
 		}
+		if(isPouring) {
+			float timeElapsed = Time.time - lastDispenseTime;
+			if(timeElapsed >= timeBetweenDispenses)
+				Pour();
+		}
+	}
+	
+	private void DoRotateTowards(Quaternion desiredRotation) {
+		float step = tiltSpeed * Time.deltaTime;
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, 
+																									desiredRotation, 
+																									step);
 	}
 
+	private bool IsDoneRotatingTowards(Quaternion desiredRotation) {
+			float currentAngle = Quaternion.Angle(desiredRotation, 
+																						gameObject.transform.rotation);
+			if(currentAngle <=0) 
+				return true;
+			else 
+				return false;
+	}
 
 	private void StartTilting() {
-		lerpStartRotation = gameObject.transform.rotation;
-		lerpStartTime = Time.time + Time.deltaTime;
-		lerpTime = GetLerpTime(tiltTime, lerpStartRotation, pouringRotation);
-		Debug.Log("Tilt Time: " + lerpTime);
 		isUntilting = false;
+		isPouring = false;
 		isTilting = true;
 		Debug.Log("Started tilting ingredient");
 	}
 
 	private void StartUntilting() {
-		lerpStartRotation = gameObject.transform.rotation;
-		lerpTime = GetLerpTime(untiltTime, lerpStartRotation, defaultRotation);
-		Debug.Log("Untilt Time: " + lerpTime);
-		lerpStartTime = Time.time;
 		isTilting = false;
+		isPouring = false;
 		isUntilting = true;
 		Debug.Log("Stoped tilting ingredient");
 	}
 
-	private float GetLerpTime(float fullTime, 
-													 Quaternion currentRotation, 
-													 Quaternion desiredRotation) {
-		float rotationDifference = Quaternion.Angle(currentRotation, 
-																								desiredRotation);
-		Debug.Log("difference: " + rotationDifference);
-		Debug.Log("full angle: " + fullPourAngle);
-		return fullTime * (rotationDifference / fullPourAngle);
+	private void StartPouring() {
+		isPouring = true;
+		Pour();
 	}
 
-	private bool LerpIt(Quaternion desiredRotation) {
-			bool finishedLerping;
-			float timeElapsed = (Time.time - lerpStartTime);
-			float progress = 1 - ( (lerpTime - timeElapsed) / lerpTime );
-			if(progress < 1f) {
-				gameObject.transform.rotation = Quaternion.Lerp(lerpStartRotation, 
-																												desiredRotation, 
-																												progress);
-				finishedLerping = false;
-			}
-			else 
-				finishedLerping = true;
-			return finishedLerping;
+	private void Pour() {
+		Dispense();
+		lastDispenseTime = Time.time;
 	}
 
+
+	// Callbacks
 	void OnTriggerEnter2D(Collider2D col) {
 		if(col.gameObject.tag == "Pour Zone")
 			StartTilting();	
@@ -92,5 +87,9 @@ public class PourableDispenser : StationaryDispenser {
 	void OnTriggerExit2D(Collider2D col) {
 		if(col.gameObject.tag == "Pour Zone")
 			StartUntilting();	
+	}
+
+	void OnMouseUp() {
+		StartUntilting();
 	}
 }

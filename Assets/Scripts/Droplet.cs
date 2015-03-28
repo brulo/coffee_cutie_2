ef9f745dbecf19e3c71a59b231c48bdf5fa7b62f
brulo <bruce.lott@gmail.com>
2015-03-28 14:34:31 -0700

    Make droplet not add ingredient to drink if drink's not upright

--- Assets/Scripts/Droplet.cs
+++ Assets/Scripts/Droplet.cs
@@ -6,9 +6,15 @@ public class Droplet : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col) {
 		if(col.gameObject.tag == "Drink") { 
-			Debug.Log("Droplet hit drink!");
-			col.gameObject.GetComponent<DrinkForCustomer>().drink.AddIngredient(ingredient);
-			Destroy(this.gameObject);
+			Quaternion drinkRotation = col.gameObject.transform.rotation;
+			Quaternion uprightRotation = Quaternion.Euler(0, 0, 0);
+			float angleFromUpright = Quaternion.Angle(uprightRotation,
+																								drinkRotation);
+			if( (angleFromUpright > -5f) & (angleFromUpright < 5f) ) {
+				Debug.Log("Droplet hit drink!");
+				col.gameObject.GetComponent<DrinkForCustomer>().drink.AddIngredient(ingredient);
+				Destroy(this.gameObject);
+			}
 		}
 		else if(col.gameObject.tag == "Counter") {
 			Debug.Log("Droplet hit counter!");

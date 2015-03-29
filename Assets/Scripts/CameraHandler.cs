cd3b5c0c8d77b968f9305a3b95e5f1d405122130
brulo <bruce.lott@gmail.com>
2015-03-29 14:31:40 -0700

    Implement naive screen move

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -4,36 +4,66 @@ using System.Collections;
 public class CameraHandler : MonoBehaviour {
 	public int numberOfScreens = 2;
 	private int currentScreen = 0;
-	public float moveAmount;
+	private float xMoveAmount;
+	private bool isMoving = false;
+
+	private Vector3 lerpStartPosition;
+	private Vector3 lerpEndPosition;
+
+	private float lerpTime = 1f;
+	private float currentLerpTime;
 
 	void Start() {
-		Vector3 thing = new Vector3(Screen.width, Screen.height, 0);
-
-		Vector3 point = Camera.main.ScreenToWorldPoint(thing);
-		moveAmount = point.x * 2;
+		Vector3 screenDimsInPixels = new Vector3(Screen.width, 
+																						 Screen.height, 0);
+		Vector3 dimsInWorldPoint = Camera.main.ScreenToWorldPoint(
+				screenDimsInPixels);
+		xMoveAmount = dimsInWorldPoint.x * 2;
 	}
 
-	public void MoveLeftAScreen() {
-		if(currentScreen > 0) {
-			Debug.Log("move left");
-			MoveCameraX(-moveAmount);
-			currentScreen--;
+	void Update() {
+		if(isMoving) {
+			bool finishedLerping = LerpIt();
+			if(finishedLerping) {
+				Debug.Log("Finished moving to screen " + currentScreen);
+				isMoving = false;
+			}
 		}
 	}
 
-	public void MoveRightAScreen() {
-		if(currentScreen < numberOfScreens - 1) {
-			Debug.Log("move right");
-			MoveCameraX(moveAmount);
-			currentScreen++;
+	private bool LerpIt() {
+		bool finishedLerping = false;
+
+		currentLerpTime += Time.deltaTime;
+		if(currentLerpTime > lerpTime) {
+			currentLerpTime = lerpTime;
+			finishedLerping = true;
 		}
+		float progress = currentLerpTime / lerpTime;
+		transform.position = Vector3.Lerp(lerpStartPosition, 
+																			lerpEndPosition, 
+																			progress);
+
+		return finishedLerping;
 	}
 
-	public void MoveCameraX(float moveAmount) {
-		Debug.Log("moved");
-		Vector3 currentPosition = transform.position;
-		Vector3 newPosition = currentPosition;
-		newPosition.x = newPosition.x + moveAmount;
-		transform.position = newPosition;
+	public void MoveFromCurrentScreen(int screensToMove) {
+		int newScreen = currentScreen + screensToMove;
+		Debug.Log("newScreen " + newScreen);
+		if( (newScreen > -1) && (newScreen < numberOfScreens) ) {
+			Debug.Log("Begin moving to screen " + newScreen);
+
+			currentScreen = newScreen;
+			isMoving = true;
+
+			currentLerpTime = 0f;
+
+			lerpStartPosition = transform.position;
+
+			Vector3 currentPosition = transform.position;
+			Vector3 newPosition = currentPosition;
+			newPosition.x = newPosition.x + (xMoveAmount * screensToMove);
+			lerpEndPosition = newPosition;
+		}
 	}
 }

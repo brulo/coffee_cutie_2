ab8bf7b0a15b286259872b76a9e76e56abab0665
brulo <bruce.lott@gmail.com>
2015-04-19 14:57:46 -0700

    Add recipe book gui

--- Assets/Scripts/RecipeBook.cs
+++ Assets/Scripts/RecipeBook.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 
 public class RecipeBook {
+	private Recipe[] allRecipes = new Recipe[] {
+		GlassOfMilk, GlassOfRegularSyrup 
+	};
+	public Recipe[] AllRecipes { get { return allRecipes; } }
 
 	public static Recipe GlassOfMilk {
 		get {
@@ -11,10 +15,28 @@ public class RecipeBook {
 			};
 
 			IngredientName[] specificIngredients = new IngredientName[] { 
-
 			};
 
 			Recipe recipe = new Recipe("Glass of Milk",
+																 cup,
+																 ingredientTypes,
+																 specificIngredients);
+			return recipe;
+		}
+	}
+
+	public static Recipe GlassOfRegularSyrup {
+		get {
+			IngredientName cup = IngredientName.ColdCup;
+
+			IngredientType[] ingredientTypes = new IngredientType[] { 
+			};
+
+			IngredientName[] specificIngredients = new IngredientName[] { 
+				IngredientName.RegularSyrup, IngredientName.RegularSyrup 
+			};
+
+			Recipe recipe = new Recipe("Glass of Syrup",
 																 cup,
 																 ingredientTypes,
 																 specificIngredients);
---
+++ Assets/Scripts/RecipeBookGui.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+/* using System; */
+
+public class RecipeBookGui : MonoBehaviour {
+	public Text leftPageText;
+	public Text rightPageText;
+	RecipeBook recipeBook = new RecipeBook();
+	private int leftPageIndex = 0;
+
+	void Start() {
+			leftPageText.text = recipeBook.AllRecipes[leftPageIndex].RecipeBookText;
+			rightPageText.text = recipeBook.AllRecipes[leftPageIndex+1].RecipeBookText;
+	}
+
+	// Use positive/negative number to flip that many pages left/right.
+	public void TurnPage(int numPageTurns) {
+		int numPagesToTurn = numPageTurns * 2;
+		int newPageIndex = leftPageIndex + numPagesToTurn;
+
+		if((newPageIndex > 0) & (newPageIndex < recipeBook.AllRecipes.Length)) {
+			leftPageIndex = newPageIndex;
+			leftPageText.text = recipeBook.AllRecipes[leftPageIndex].RecipeBookText;
+
+			int rightPageIndex = leftPageIndex + 1;
+			if(rightPageIndex < recipeBook.AllRecipes.Length) {
+				rightPageText.text = recipeBook.AllRecipes[rightPageIndex].RecipeBookText;
+			}
+			else {
+				rightPageText.text = "";
+			}
+		}
+	}
+}

2516ac69a75c08171839c6ebb5a56384b15984e7
brulo <bruce.lott@gmail.com>
2015-05-03 16:00:05 -0700

    spawn cat callers periodically

--- Assets/Scripts/CatCallerHandler.cs
+++ Assets/Scripts/CatCallerHandler.cs
@@ -3,9 +3,24 @@ using System.Collections;
 
 public class CatCallerHandler : MonoBehaviour {
 	public GameObject catCallerPrefab;
+	public GameState gameState;
+	private float timeBetweenSummons = 30f;
 
 	void Start() {
+		gameState = GameObject.Find("Game State").GetComponent<GameState>();
+		StartCoroutine(SummonLoop(timeBetweenSummons));
+	}
+	
+	public void SummonCatcaller() {
 		GameObject catCaller = Instantiate(catCallerPrefab);
 		catCaller.transform.parent = GameObject.Find("World Position Canvas").transform;
 	}
+
+	IEnumerator SummonLoop(float seconds) {
+		while(true) {
+			yield return new WaitForSeconds(seconds);
+			SummonCatcaller();
+		}
+	}
+
 }

fc9b53685b526f5d557c4face64a3a387f612efe
brulo <bruce.lott@gmail.com>
2015-05-06 23:57:27 -0700

    git shit

--- Assets/Scripts/RecipeBookBlink.cs
+++ Assets/Scripts/RecipeBookBlink.cs
@@ -11,11 +11,7 @@ public class RecipeBookBlink : MonoBehaviour {
 	}
 
 	IEnumerator BlinkImage() {
-<<<<<<< HEAD
 		for(int i = 0; i < 3; i++) {
-=======
-		for(int i = 0; i < 5; i++) {
->>>>>>> 1acd5c016a48fbd1b57e9aa691f7bf0d07599c62
 			image.color = Color.grey;
 			yield return new WaitForSeconds(.4f);
 			image.color = Color.white;

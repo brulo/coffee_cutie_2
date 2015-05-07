1acd5c016a48fbd1b57e9aa691f7bf0d07599c62
brulo <bruce.lott@gmail.com>
2015-05-06 23:48:09 -0700

    make recipe book blink when game starts

---
+++ Assets/Scripts/RecipeBookBlink.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RecipeBookBlink : MonoBehaviour {
+	private Image image;
+
+	void Start() {
+		image = GetComponent<Image>();
+		StartCoroutine(BlinkImage());
+	}
+
+	IEnumerator BlinkImage() {
+		for(int i = 0; i < 5; i++) {
+			image.color = Color.grey;
+			yield return new WaitForSeconds(.4f);
+			image.color = Color.white;
+			yield return new WaitForSeconds(.4f);
+		}
+	}
+
+}
